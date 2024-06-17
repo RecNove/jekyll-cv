a494b97c5eed1f398cbae54af30a2d9461b2610f
Gabriel Amorim <gabriel._amorim@hotmail.com>
2024-06-17 11:39:20 -0300

    testando botão de download

--- assets/javascript/downloadPDF.js
+++ assets/javascript/downloadPDF.js
@@ -1,30 +1,8 @@
-function downloadPDF(){
-    console.log("fui chamado");
-    var iframe = document.getElementsByClassName('container')[0];
-
-if (!iframe) {
-    console.error('Iframe not found');
-    return;
-}
-
-var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-if (!iframeDoc) {
-    console.error('Cannot access iframe document');
-    return;
-}
-
-var blob = new Blob([iframeDoc.documentElement.outerHTML], { type: 'text/html' });
-
-var url = window.URL.createObjectURL(blob);
-var a = document.createElement('a');
-a.href = url;
-a.download = 'iframe_content.html';
-
-document.body.appendChild(a);
-
-a.click();
-
-document.body.removeChild(a);
-window.URL.revokeObjectURL(url);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var downloadButton = document.getElementById('download');
+    console.log("fui chamado;")
+    downloadButton.addEventListener('click', function() {
+      window.print();
+    });
+  });
+  
